9b242524e2aaaa6aa33b0670f5fc9d86229b23b7
jinwei14 <jinwei14@gmail.com>
2018-02-14 20:08:14 +0000

    phrase1 add the gun and the player object and scripts

---
+++ Phrase1_onlyGun/Projectile.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    float speed = 10;
+
+
+    public void setSpeed(float newSpeed) {
+        speed = newSpeed;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate (Vector3.forward * Time.deltaTime * speed);
+	}
+}
